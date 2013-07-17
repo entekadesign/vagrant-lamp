8757630612ca9c87704b9ade247567d6163de0c1
Marco Tomaschett <marco@fatcatchdesign.com>
2013-07-17 08:58:32 -0400

    initial commit

---
+++ puppet/manifests/site.pp
@@ -0,0 +1,40 @@
+node 'fc.fatcatchdesign.dev' {
+            class { 'mysql::server':
+                    config_hash => { 'root_password' => 'tundra' },
+            }
+            include mysql::php
+
+            # configuring apache
+            include apache
+            include apache::mod::php
+
+            apache::vhost { $::fqdn:
+                    port => '80',
+                    docroot => '/var/www/htdocs',
+                    require => File['/var/www/htdocs'],
+                    priority      => '1',
+            }
+
+            # setting up the document root
+            file { ['/var/www/htdocs']:
+                    ensure => directory,
+            }
+
+            # open port for web
+            firewall { '0100-INPUT ACCEPT 80' :
+                action => 'accept',
+                dport  => 80,
+                proto  => 'tcp',
+            }
+
+            
+            # for EPEL packages
+            exec { "grap-epel":
+              command => "/bin/rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm",
+              creates => "/etc/yum.repos.d/epel.repo",
+              alias   => "grab-epel",
+            }
+
+            # phpmyadmin from EPEL; no puppetlabs module available
+            include phpmyadmin
+}
